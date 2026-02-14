2b69d02f5b2e34390f602c8fb0d2a0a16d29f833
Kelly <kellybytes.ca@gmail.com>
2026-02-13 18:40:59 -0700

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,43 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental properties',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
+
+// import { Geist, Geist_Mono } from "next/font/google";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
