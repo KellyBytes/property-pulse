60393042b7f8639c5109506ad20dffd95b1ecb6d
Kelly <kellybytes.ca@gmail.com>
2026-02-17 12:45:15 -0700

    Add Property Details components

---
+++ components/PropertyHeaderImage.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+const PropertyHeaderImage = ({ image }) => {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <div className="relative h-[400px] w-full">
+            <Image
+              src={`/images/properties/${image}`}
+              alt=""
+              className="object-cover"
+              fill
+              size="100vw"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default PropertyHeaderImage;
